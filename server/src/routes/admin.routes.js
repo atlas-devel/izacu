5969b86607d73e3aace1668d28d996536d1cedbb
Leon <irakaramale@gmail.com>
2025-11-21 23:54:21 +0200

    untrack server

--- server/src/routes/admin.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createMovie } from "../controllers/admin.js";
-
-const AdminRoutes = express.Router();
-
-AdminRoutes.post("/admin/create-movie/:adminId", createMovie);
-
-export default AdminRoutes;
