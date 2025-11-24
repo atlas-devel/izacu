c7c8a469fa4ef5651cee4f5432000d986d0b7480
Leon <irakaramale@gmail.com>
2025-11-24 22:03:36 +0200

    final setup

--- server/src/controllers/auth.controller.js
+++
@@ -1,9 +0,0 @@
-export const changePassword = (req, res) => {
-  const { password, email } = req.body;
-  if (!password || !email) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Password and email are required" });
-  }
-    
-};
--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,13 +1,6 @@
 import express from "express";
-import { createMovie } from "../controllers/admin.js";
 import parse from "../utils/cloudinary.js";
 
 const AdminRoutes = express.Router();
-
-AdminRoutes.post(
-  "/admin/create-movie/:adminId",
-  parse.array("image"),
-  createMovie
-);
 
 export default AdminRoutes;
